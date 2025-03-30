4f57d738202f2097f0684a42f91181c270c7d598
telly497 <mmilitello4@gmail.com>
2025-03-30 22:38:38 +0900

    added google analytics 2

---
+++ scripts/analytics.js
@@ -0,0 +1,14 @@
+// Google Analytics Global Site Tag
+(function(){
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-Z4LG6R6QX4";
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-Z4LG6R6QX4');
+})();

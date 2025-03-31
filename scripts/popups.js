aff4d6acb2c42cb19e03c05a449361f017631d55
telly497 <mmilitello4@gmail.com>
2025-04-01 02:06:50 +0900

    Add link to Dixieland website in popup

--- scripts/popups.js
+++ scripts/popups.js
@@ -57,6 +57,7 @@ dixieland: `
     <div class="popup-header">
         <img src='assets/shields/dixieland.png' class='popup-shield'>
         <h3>The Dixieland Tribe</h3>
+        <p style="margin-top:5px;"><a href="https://dixiefirst.com" target="_blank" style="font-size:0.9em; text-decoration:underline;">Visit the Dixieland Website</a></p>
     </div>
     <p>Dixieland preserves the culture of the old South, marked by aristocratic traditions, agriculture, and the memory of Cavalier forebears.</p>
     <ul>
